219214a4e2d54d9ae2c5e4b8cdef4ee8d6bbedd4
MahakHolkar <66208808+mahakholkar@users.noreply.github.com>
2022-02-10 15:52:31 +0530

    Intial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+# Add Implementation
+def add(x,y):
+	return x+y
+
+# Subtract Implementation
+def subtract(x,y):
+	return x-y          #on master branch
+	
+# Multiply Implementation
+def multiply(x,y):
+	return x*y
+
+# Divide Implementation	
+def divide(x,y):
+	if y==0:
+		return DIVIDE_BY_O_ERROR
+	else:
+		return x/y
